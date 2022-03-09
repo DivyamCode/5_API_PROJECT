d5fa077f14d7b5bcb2f7d621f08873e507e8d211
DivyamCode <88375037+DivyamCode@users.noreply.github.com>
2022-03-09 21:43:46 +0530

    Update app.js

--- backened/src/app.js
+++ backened/src/app.js
@@ -112,35 +112,33 @@ app.post("/insert",async (req,res)=>{
 app.get('/login',(req,res)=>{
     res.sendFile(path.join(__dirname,"../../frontened/login.html"))
 })
-// app.post("/login",async (req,res)=>{
-//     try{
-//         const email = req.body.email;
-//         const password = req.body.password;
-//         // const lph = bcrypt.hashSync(password,10);
+app.post("/login",async (req,res)=>{
+   try{
+      const email = req.body.email;
+       const password = req.body.password;
+       // const lph = bcrypt.hashSync(password,10);
         
-//         const useremail = await Register.findOne({user_email:email});
-//         // res.send(useremail.password);
-//         console.log(useremail.user_password);
+       const useremail = await Register.findOne({user_email:email});
+       // res.send(useremail.password);
+        console.log(useremail.user_password);
 
 
-//         //----------->>>FOR COMAPRING BCRYPT ON LOGIN
+        //----------->>>FOR COMAPRING BCRYPT ON LOGIN
         
-//         const isokay = bcrypt.compare(password,useremail.user_password);
+        const isokay = bcrypt.compare(password,useremail.user_password);
     
-//         if(isokay){
-//             res.send("you r signed up");
-//             //cookie set
+        if(isokay){
+            res.send("you r signed up");
+           //cookie set
             
-//         }else{
-//             res.send("you entered wrong password")
-//         };
+        }else{
+            res.send("you entered wrong password")
+        };
 
-//     }catch{
-//         res.status(400).send("invalid email");
-//     }
-
-
-// })
+     }catch{
+         res.status(400).send("invalid email");
+    }
+ })
 
 //LISTENING SERVER ON PORT
 app.listen(port,()=>{
